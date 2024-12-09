93b276d4112d2066f9f6d3a6916335a0b4ed5e1a
Abinash6000 <abinashdash6000@gmail.com>
2024-12-09 12:27:18 +0530

    first commit

---
+++ app/src/main/java/com/project/firbasegooglesigninwithjetpackcompose/presentation/sign_in/SignInState.kt
@@ -0,0 +1,6 @@
+package com.project.firbasegooglesigninwithjetpackcompose.presentation.sign_in
+
+data class SignInState(
+    val isSignInSuccessful: Boolean = false,
+    val signInError: String? = null
+)
